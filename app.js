4869b2b513611b8104de8b24d083b81b10bd9517
TheWhiteHackers <nurefsanbera@gmail.com>
2022-07-28 16:30:25 -0500

    almost done

--- app.js
+++ app.js
@@ -4,9 +4,51 @@ this.author = author;
 this.isbn = isbn;
 }
 
-document.getElementById("submit").addEventListener("submit", function (e) {
-    // e.preventDefult()
-    const title = document.getElementById("title").value;
-    console.log(title);
-});
+function UI() {}
 
+UI.prototype.addBook=function(mybook){
+    const tableList = document.getElementById("book-list");
+    const row = document.createElement("tr");
+    row.innerHTML=`
+    <td>${mybook.title}</td>
+    <td>${mybook.author}</td>
+    <td>${mybook.isbn}</td>
+    <td>X</td>`
+
+    tableList.appendChild(row);
+}
+
+UI.prototype.clearfield=function(){
+    document.getElementById("title").value="";
+    document.getElementById("author").value="";
+    document.getElementById("isbn").value="";
+
+}
+
+
+
+// document.getElementById("book-form").addEventListener("submit", function(e) {
+//     e.preventDefult();
+//     const title = document.getElementById("title").value;
+//     const author = document.getElementById("author").value;
+//     const isbn = document.getElementById("isbn").value;
+//     console.log(title, author, isbn);
+// });
+
+document.getElementById("book-form").addEventListener("submit",function(e){
+    e.preventDefault();
+    const title=document.getElementById("title").value;
+    const author=document.getElementById("author").value;
+    const isbn=document.getElementById("isbn").value;
+    console.log(title,author,isbn)
+
+    const MyBookList = new Book(title, author, isbn)
+    console.log(MyBookList);
+
+    const newUI = new UI();
+    console.log(newUI);
+
+    newUI.addBook(MyBookList)
+    newUI.clearfield()
+
+})
